c6e22ae91b8fec55ef22babad51d87f2e4538022
lezhang-thu <lezhang.thu@gmail.com>
2026-07-13 17:30:12 +0800

    run scripts

--- run-cuda0.sh
+++ run-cuda0.sh
@@ -1,20 +1,39 @@
 set -ex
-#for a in /sys/bus/pci/devices/*; do echo 0 | sudo tee -a $a/numa_node; done
-#strings=("Qbert" "RoadRunner")
-#strings=("Gopher" "Hero" "Jamesbond" "Kangaroo" "Krull" "KungFuMaster" "MsPacman" "Pong" "PrivateEye" "Qbert" "RoadRunner" "Seaquest" "UpNDown")
-strings=("Pong")
-seed=1807987954 
-for ((j=11;j<=11;j++));
-do
-for game_name in "${strings[@]}";
-do
-    echo "iteration ${j}"
-    CUDA_VISIBLE_DEVICES=0 python -m bbf.train \
-        --agent=BBF \
-        --gin_files=bbf/configs/BBF-100K.gin \
-        --gin_bindings="DataEfficientAtariRunner.game_name=\"${game_name}\"" \
-        --run_number=${j} #\
-        #--agent_seed=$seed --eval_only=True --no_seeding=False
+strings=(
+	"Alien"
+	"Amidar"
+	"Assault"
+	"Asterix"
+	"BankHeist"
+	"BattleZone"
+	"Boxing"
+	"Breakout"
+	"ChopperCommand"
+	"CrazyClimber"
+	"DemonAttack"
+	"Freeway"
+	"Frostbite"
+	#"Gopher"
+	#"Hero"
+	#"Jamesbond"
+	#"Kangaroo"
+	#"Krull"
+	#"KungFuMaster"
+	#"MsPacman"
+	#"Pong"
+	#"PrivateEye"
+	#"Qbert"
+	#"RoadRunner"
+	#"Seaquest"
+	#"UpNDown"
+)
+for ((j = 11; j <= 11; j++)); do
+	for game_name in "${strings[@]}"; do
+		echo "iteration ${j}"
+		CUDA_VISIBLE_DEVICES=0 python -m bbf.train \
+			--agent=BBF \
+			--gin_files=bbf/configs/BBF-100K.gin \
+			--gin_bindings="DataEfficientAtariRunner.game_name=\"${game_name}\"" \
+			--run_number=${j}
+	done
 done
-done
-#rm -rf /tmp/online_rl/bbf/cuda0/$seed
---
+++ run-cuda1.sh
@@ -0,0 +1,39 @@
+set -ex
+strings=(
+	#"Alien"
+	#"Amidar"
+	#"Assault"
+	#"Asterix"
+	#"BankHeist"
+	#"BattleZone"
+	#"Boxing"
+	#"Breakout"
+	#"ChopperCommand"
+	#"CrazyClimber"
+	#"DemonAttack"
+	#"Freeway"
+	#"Frostbite"
+	"Gopher"
+	"Hero"
+	"Jamesbond"
+	"Kangaroo"
+	"Krull"
+	"KungFuMaster"
+	"MsPacman"
+	"Pong"
+	"PrivateEye"
+	"Qbert"
+	"RoadRunner"
+	"Seaquest"
+	"UpNDown"
+)
+for ((j = 11; j <= 11; j++)); do
+	for game_name in "${strings[@]}"; do
+		echo "iteration ${j}"
+		CUDA_VISIBLE_DEVICES=1 python -m bbf.train \
+			--agent=BBF \
+			--gin_files=bbf/configs/BBF-100K.gin \
+			--gin_bindings="DataEfficientAtariRunner.game_name=\"${game_name}\"" \
+			--run_number=${j}
+	done
+done
